d3a78d52c38c41fb3cd46c3d53091b48a7918025
DeadlNek <153514866+mv-Levi@users.noreply.github.com>
2024-11-13 22:34:18 +0300

    Реализовал модели данных, DTO, мапперы и логику приложения

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Data;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class ItemDto {
+    private Long id;
+    private String name;
+    private String description;
+    private Boolean available;
+    private Long ownerId;
+    private Boolean isRequest;
+
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * TODO Sprint add-controllers.
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    Long id;
+    String name;
+    String email;
 }
