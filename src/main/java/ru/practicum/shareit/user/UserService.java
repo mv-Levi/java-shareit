0fa887046fc6185fe875a7a27aa417b3a9727b21
DeadlNek <153514866+mv-Levi@users.noreply.github.com>
2024-11-13 22:46:58 +0300

    Checkstyle3

--- src/main/java/ru/practicum/shareit/user/UserService.java
+++ src/main/java/ru/practicum/shareit/user/UserService.java
@@ -6,8 +6,13 @@ import java.util.List;
 
 public interface UserService {
     UserDto createUser(UserDto userDto);
+
     List<UserDto> getAllUsers();
+
     UserDto getUser(Long userId);
+
     UserDto updateUser(Long userId, UserDto userDto);
+
     void deleteUser(Long userId);
+
 }
