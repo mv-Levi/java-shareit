2f41659cf995884c6499286f0defd48e8fd447ca
DeadlNek <153514866+mv-Levi@users.noreply.github.com>
2024-11-13 22:44:28 +0300

    Checkstyle1

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface ItemService {
     ItemDto addItem(Long userId, ItemDto itemDto);
+
     ItemDto updateItem(Long itemId, Long userId, ItemDto itemDto);
+
     ItemDto getItem(Long itemId);
+
     List<ItemDto> getItemsByOwner(Long userId);
+
     List<ItemDto> searchItems(String text);
 }
