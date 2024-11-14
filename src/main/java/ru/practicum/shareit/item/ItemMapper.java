6e22936b4ecd874507eb8c2efd103d0a8b822142
DeadlNek <153514866+mv-Levi@users.noreply.github.com>
2024-11-14 19:32:03 +0300

    Убрал комменты

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -6,31 +6,28 @@ import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
 
 public class ItemMapper {
-    // Преобразование из Item в ItemDto
     public static ItemDto toItemDto(Item item) {
         ItemDto itemDto = new ItemDto();
 
-        itemDto.setId(item.getId());                      // Устанавливаем id
-        itemDto.setName(item.getName());                   // Устанавливаем название
-        itemDto.setDescription(item.getDescription());     // Устанавливаем описание
-        itemDto.setAvailable(item.getAvailable());         // Устанавливаем статус доступности
-        itemDto.setOwnerId(item.getOwner() != null ? item.getOwner().getId() : null); // Устанавливаем ID владельца
-        itemDto.setIsRequest(item.getRequest() != null);   // Проверяем, был ли запрос
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.getAvailable());
+        itemDto.setOwnerId(item.getOwner() != null ? item.getOwner().getId() : null);
+        itemDto.setIsRequest(item.getRequest() != null);
 
         return itemDto;
     }
 
-
-    // Преобразование из ItemDto в Item
     public static Item toItem(ItemDto itemDto, User owner, ItemRequest request) {
         Item item = new Item();
 
-        item.setId(itemDto.getId());               // Устанавливаем id
-        item.setName(itemDto.getName());           // Устанавливаем название
-        item.setDescription(itemDto.getDescription()); // Устанавливаем описание
-        item.setAvailable(itemDto.getAvailable()); // Устанавливаем доступность
-        item.setOwner(owner);                      // Устанавливаем владельца
-        item.setRequest(request);                  // Устанавливаем запрос, если он есть
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setOwner(owner);
+        item.setRequest(request);
 
         return item;
     }
