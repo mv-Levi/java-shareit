b3b4210e4295389b9c75bd295d628b9129238d4d
Nikita <153514866+mv-Levi@users.noreply.github.com>
2025-02-24 09:21:44 +0300

    Реализовал требования ТЗ 15
    Добавил класс Comment и обновил класс item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 import ru.practicum.shareit.request.ItemRequest;
 import ru.practicum.shareit.user.User;
@@ -8,12 +9,28 @@ import ru.practicum.shareit.user.User;
  * TODO Sprint add-controllers.
  */
 @Data
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "available", nullable = false)
     private Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", nullable = false)
     private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id")
     private ItemRequest request;
 
 }
