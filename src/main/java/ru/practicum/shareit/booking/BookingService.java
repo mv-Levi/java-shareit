db868d2430ab30941f660e22682fa88f17aa6dd2
Nikita <153514866+mv-Levi@users.noreply.github.com>
2025-02-24 09:54:46 +0300

    Checkstyle 1

--- src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 public interface BookingService {
     BookingDto createBooking(Long userId, BookingDto bookingDto);
+
     BookingDto updateBookingStatus(Long bookingId, boolean approved, Long userId);
+
     BookingDto getBookingById(Long bookingId, Long userId);
+
     List<BookingDto> getBookingsByUser(Long userId, String state);
+
     List<BookingDto> getBookingsByOwner(Long userId, String state);
+
 }
